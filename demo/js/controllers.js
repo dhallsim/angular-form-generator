9c55fdb4d2a63ac96a85fb82b10280e9711e2cb6
Derek Hall <dsh8837@gmail.com>
2013-09-16 01:10:26 -0400

    major refactor on json model and form factory refactoring project structure

---
+++ demo/js/controllers.js
@@ -0,0 +1,388 @@
+'use strict';
+
+
+/*
+	Brainstorm: Types of form fields
+
+	1. Input (Text, email, telephone, url,...)
+	2. Textarea
+	3. Select
+	4. Select (Multiple)
+	5. Checkboxes
+	6. Radio Buttons
+
+	Model:
+	{
+		// List of classes for the form
+		classes: [],
+
+		// Object containing key-pair values to use as attributes on the form
+		attributes: {},
+
+		// Form fields
+		fields: [
+			// Sample input
+			{
+				label: {
+					// Name of label (this is also what is displayed in the HTML)
+					name: '',
+
+					// List of classes for the label
+					classes: [],
+
+					// Attributes for the label
+					attributes: {},
+
+					// Boolean determining whether label will wrap the field or not
+					wrapField: false					
+				},
+
+				// Type of form field
+				// types: [
+
+					// All input types supported
+					input: [
+						'text',
+						'email',
+						'password',
+						'number',
+						'telephone',
+						'url'
+					],
+
+					'textarea',
+					'radio',
+					'checkbox',
+					'select' (including multiple)
+				],
+
+				type: 'text',
+				model: 'person.name',
+				placeholder: 'Enter name here',
+				attributes: {},
+				required: false,
+				group: 1
+			}
+		],
+
+		// Groups for fields. These represent a container for a single field or multiple fields.
+		// The HTML for a group will be a div wrapping the field(s).
+		groups: [
+			{
+				// id that will be referenced by a field object
+				id: '',
+
+				// Name of group
+				name: '',
+
+				// List of classes that will be applied to the group
+				classes: [],
+
+				// Object containing key-pair values to use as atrributes for group
+				attributes: {}
+			}
+		]
+	}
+*/
+
+
+function indexCtrl($scope) {
+	$scope.countries = [
+		{
+			name: 'United States',
+			code: 'US'
+		},
+		{
+			name: 'Japan',
+			code: 'JP'
+		},
+		{
+			name: 'Mexico',
+			code: 'MX'
+		},
+		{
+			name: 'Canada',
+			code: 'CA'
+		}
+	];
+
+	$scope.bands = [
+		{
+			name: 'August Burns Red',
+			genre: 'metalcore'
+		},
+		{
+			name: 'Blink-182',
+			genre: 'pop punk'
+		},
+		{
+			name: 'Tool',
+			genre: 'progressive metal'
+		},
+		{
+			name: 'Senses Fail',
+			genre: 'emo'
+		}
+	];
+
+	$scope.person = {
+		codingExperience: 'junior'
+	};
+
+	$scope.form = {
+		classes: [],
+		attributes: {
+			id: 'ballsack'
+		},
+		fields: [
+			{
+				label: {
+					name: 'Name',
+					classes: [],
+					wrapField: false
+				},
+				type: 'text',
+				model: 'person.name',
+				placeholder: 'Enter name here',
+				classes: ['form-control'],
+				required: false,
+				group: 1
+			},
+			{
+				label: {
+					name: 'Age',
+					classes: [],
+					wrapField: false
+				},
+				type: 'number',
+				model: 'person.age',
+				placeholder: 'Enter Age here',
+				classes: ['form-control'],
+				required: true,
+				group: 1,
+				attributes: {
+					min: 1,
+					max: 120
+				}
+			},
+			{
+				label: {
+					name: 'Password',
+					classes: [],
+					wrapField: false
+				},
+				type: 'password',
+				model: 'person.password',
+				placeholder: 'Enter password here',
+				classes: ['form-control'],
+				required: true,
+				group: 1
+			},
+			{
+				label: {
+					name: 'Email',
+					classes: [],
+					wrapField: false
+				},
+				type: 'email',
+				model: 'person.email',
+				placeholder: 'Enter email here',
+				classes: ['form-control'],
+				required: true,
+				group: 1
+			},
+			{
+				label: {
+					name: 'Website',
+					classes: [],
+					wrapField: false
+				},
+				type: 'url',
+				placeholder: 'Enter url here',
+				model: 'person.website',
+				classes: ['form-control'],				
+				required: true,
+				group: 1
+			},
+			{
+				label: {
+					name: 'About Me',
+					classes: [],
+					wrapField: false
+				},
+				type: 'textarea',
+				model: 'person.description',
+				placeholder: 'Enter description here',
+				classes: ['form-control'],
+				required: true,
+				group: 1
+			},
+			{
+				label: {
+					name: 'Coding Experience',
+					classes: [],
+					wrapField: false
+				},
+				type: 'radio',
+				model: 'person.codingExperience',
+				group: 1,
+				options: [
+				{
+					label: {
+						name: 'Junior',
+						classes: [],
+						wrapField: true
+					},
+					value: 'junior',
+					group: 3
+				},
+				{
+					label: {
+						name: 'Intermediate',
+						classes: [],
+						wrapField: true
+					},
+					value: 'intermediate',
+					group: 3
+				},
+				{
+					label: {
+						name: 'Senior',
+						classes: [],
+						wrapField: true
+					},
+					value: 'senior',
+					group: 3
+				},
+				{
+					label: {
+						name: 'Badass',
+						classes: [],
+						wrapField: true
+					},
+					value: 'badass',
+					group: 3
+				}
+				],
+				required: true
+			},
+			{
+				label: {
+					name: 'Remember Me',
+					classes: [],
+					wrapField: true
+				},
+				type: 'checkbox',
+				model: 'person.remember',
+				group: 2
+			},
+			{
+				label: {
+					name: 'Programming Languages',
+					classes: [],
+					wrapField: false
+				},
+				type: 'checkbox',
+				model: 'person.test.testing.tested.languages',
+				options: [
+					{
+						label: {
+							name: 'Java',
+							classes: ['checkbox'],
+							wrapField: true
+						},
+						value: 'Java'
+					},
+					{
+						label: {
+							name: 'JavaScript',
+							classes: ['checkbox'],
+							wrapField: true
+						},
+						value: 'JavaScript'
+					},
+					{
+						label: {
+							name: 'C++',
+							classes: ['checkbox'],
+							wrapField: true
+						},
+						value: 'C++'
+					},
+					{
+						label: {
+							name: 'Python',
+							classes: ['checkbox'],
+							wrapField: true
+						},
+						value: 'Python'
+					}
+				],
+				required: true
+			},
+			// {
+			// 	label: {
+			// 		name: 'Country',
+			// 		classes: [],
+			// 		wrapField: true
+			// 	},
+			// 	type: 'select',
+			// 	multiple: false,
+			// 	model: 'person.country',
+			// 	required: true,
+			// 	classes: ['form-control'],
+			// 	optionsExpression: 'country.name for country in countries'
+			// },
+			{
+				label: {
+					name: 'Select Cool Bands',
+					classes: [],
+					wrapField: false
+				},
+				type: 'select',
+				multiple: true,
+				model: 'person.favoriteBands',
+				required: true,
+				classes: ['form-control'],
+				optionsExpression: 'band.name for band in bands'
+			}
+		],
+		groups: [
+			{
+				id: 1,
+				name: 'group1',
+				classes: ['form-group'],
+				attributes: {}
+			},
+			{
+				id: 2,
+				name: 'checkboxGroup',
+				classes: ['checkbox'],
+				attributes: {}
+			},
+			{
+				id: 3,
+				name: 'radioGroup',
+				classes: ['radio'],
+				attributes: {}
+			}
+		]
+	};
+
+	$scope.addField = function() {
+		var field = {
+			label: {
+				name: 'Another Input',
+				classes: ['poopy'],
+				wrapField: false,
+				attributes: {}
+			},
+			type: 'text',
+			model: 'person.ballsack',
+			placeholder: 'Enter something',
+			required: false
+		};
+
+		$scope.form.fields.push(field);
+	};
+}
---
+++ src/angular-form-generator.js
@@ -0,0 +1,335 @@
+'use strict';
+
+var formGenerator = angular.module('formGeneratorModule', []);
+
+formGenerator.factory('FormFactory', function() {
+  	var formFactory = {};
+  	var groupMap = {};
+
+	formFactory.getForm = function(form) {
+		if (!form) {
+			return '';
+		}
+
+		var fields = form.fields || [];
+		var groups = form.groups || [];
+
+		// Builing map for easy retrieval
+		for (var g = 0; g < groups.length; g++) {
+			groupMap[groups[g].id] = groups[g];
+		}
+
+		// Build form
+		var html = '<form ';
+		html += getClassesStr(form.classes);
+		html += getAttributesStr(form.attributes);
+		html += '>';
+
+		if (!fields || !fields.length) {
+			return '';
+		}
+
+		for (var i = 0; i < fields.length; i++) {
+			var field = fields[i];
+			field.classes = field.classes || [];
+
+			// Get group for field (if applicable)
+			var fieldGroup = groupMap[field.group];
+
+			// Build out group
+			if (fieldGroup) {
+				html += '<div ';
+				html += getClassesStr(fieldGroup.classes);
+				html += getAttributesStr(fieldGroup.attributes);
+				html += '>';
+			}
+
+			if (isInputType(field.type)) {
+				html += getInputHtml(field);
+			} else if (field.type === 'textarea') {
+				html += getTextareaHtml(field);
+			} else if (field.type === 'radio') {
+				html += getRadioHtml(field);
+			} else if (field.type === 'checkbox') {
+				html += (angular.isDefined(field.options) && (field.options instanceof Array)) ? getArrayCheckboxHtml(field) : getSingleCheckboxHtml(field);
+			} else if (field.type === 'select') {
+				html += getSelectHtml(field);
+			} else {
+				console.warn('Type of ' + field.type + ' not supported.');
+			}
+
+			if (fieldGroup) {
+				html += '</div>';
+			}
+		}
+
+		html += '</form>';
+
+		return html;
+	};
+
+	function isInputType(type) {
+		return (type === 'text' || type === 'email' || type === 'number' || type === 'telephone' || type === 'url' || type === 'password');
+	}
+
+	function getInputHtml(obj) {
+		if (!obj || !angular.isObject(obj)) return '';
+
+		var inputHtml = '';
+		inputHtml += '<input '; 
+		inputHtml += getAttributeStr('type', obj.type);
+		inputHtml += getAttributeStr('placeholder', obj.placeholder);
+		inputHtml += getAttributeStr('ng-required', obj.required);
+		inputHtml += getAttributeStr('ng-model', obj.model);
+		inputHtml += getClassesStr(obj.classes);
+		inputHtml += getAttributesStr(obj.attributes);
+		inputHtml += '/>';
+
+		return decorateWithLabel(obj.label, inputHtml);
+	}
+
+	function getTextareaHtml(obj) {
+		if (!obj || !angular.isObject(obj)) return '';
+
+		var textareaHtml = '';
+		textareaHtml += '<textarea ';
+		textareaHtml += getAttributeStr('placeholder', obj.placeholder);
+		textareaHtml += getAttributeStr('ng-required', obj.required);
+		textareaHtml += getAttributeStr('ng-model', obj.model);		
+    	textareaHtml += getClassesStr(obj.classes);
+    	textareaHtml += getAttributesStr(obj.attributes);
+		textareaHtml += '></textarea>';
+
+		return decorateWithLabel(obj.label, textareaHtml);
+	}
+
+	function getRadioHtml(obj) {
+		if (!obj || !angular.isObject(obj)) return '';
+
+		var radiosHtml = '';
+		for (var i = 0; i < obj.options.length; i++) {
+			var radioHtml = '';
+			radioHtml += '<input ';
+			radioHtml += getAttributeStr('type', 'radio');
+			radioHtml += getAttributeStr('value', obj.options[i].value);
+			radioHtml += getAttributeStr('ng-model', obj.model);
+			radioHtml += '/>';
+			radioHtml = decorateWithLabel(obj.options[i].label, radioHtml);
+
+			var radioGroup = groupMap[obj.options[i].group];
+			if (radioGroup) {
+				var groupHtml = '';
+				groupHtml += '<div ';
+				groupHtml += getClassesStr(radioGroup.classes);
+				groupHtml += getAttributeStr(radioGroup.attributes);
+				groupHtml += '>';
+
+				radioHtml = (groupHtml + radioHtml);
+				radioHtml += '</div>';
+			}
+
+			radiosHtml += radioHtml;
+		}
+
+		return decorateWithLabel(obj.label, radiosHtml);
+	}
+
+	function getSingleCheckboxHtml(obj) {
+		if (!obj || !angular.isObject(obj)) return '';
+
+		var checkboxHtml = '';
+		checkboxHtml += '<input ';
+		checkboxHtml += getAttributeStr('type', 'checkbox');
+		checkboxHtml += getAttributeStr('ng-model', obj.model);
+		checkboxHtml += '/>';
+
+		return decorateWithLabel(obj.label, checkboxHtml);
+	}
+
+	function getArrayCheckboxHtml(obj) {
+		if (!obj || !angular.isObject(obj)) return '';
+
+		var checkboxesHtml = '';
+		for (var i = 0; i < obj.options.length; i++) {
+			var checkboxHtml = '';
+			checkboxHtml += '<input ';
+			checkboxHtml += getAttributeStr('type', 'checkbox');
+			checkboxHtml += getAttributeStr('ng-click', 'addToCheckboxArray(\'' + obj.options[i].value + '\', \'' + obj.model + '\')');
+			checkboxHtml += '/>';
+			checkboxHtml = decorateWithLabel(obj.options[i].label, checkboxHtml);
+
+			var checkboxGroup = groupMap[obj.options[i].group];
+			if (checkboxGroup) {
+				var groupHtml = '';
+				groupHtml += '<div ';
+				groupHtml += getClassesStr(checkboxGroup.classes);
+				groupHtml += getAttributeStr(checkboxGroup.attributes);
+				groupHtml += '>';	
+
+				checkboxHtml = (groupHtml + checkboxHtml);
+				checkboxHtml += '</div>';
+			}
+
+			checkboxesHtml += checkboxHtml;
+		}
+
+		return decorateWithLabel(obj.label, checkboxesHtml);
+	}
+
+	function getSelectHtml(obj) {
+		if (!obj || !angular.isObject(obj)) return '';
+
+		var selectHtml = '';
+		selectHtml += '<select ';
+		selectHtml += getClassesStr(obj.classes);
+		selectHtml += getAttributeStr('ng-model', obj.model);
+		selectHtml += getAttributeStr('ng-required', obj.required);
+		selectHtml += getAttributeStr('ng-options', obj.optionsExpression);
+		selectHtml += (obj.multiple) ? 'multiple' : '';
+		selectHtml += '></select>';
+
+		return decorateWithLabel(obj.label, selectHtml);
+	}
+
+	function decorateWithLabel(labelObj, html) {
+		if (!labelObj || !angular.isObject(labelObj)) {
+			return html;
+		}
+
+		return (labelObj.wrapField) ? (wrapWithLabel(labelObj, html)) : (getLabelHtml(labelObj) + html);
+	}
+
+	function getLabelHtml(obj) {
+		if (!obj || !angular.isObject(obj)) return '';
+
+		var labelHtml = '<label ';
+		labelHtml += getClassesStr(obj.classes);
+		labelHtml += getAttributesStr(obj.attributes);
+		labelHtml += '>' + obj.name + '</label>';
+
+		return labelHtml;
+	}
+
+	function wrapWithLabel(labelObj, fieldHtml) {
+		if (!labelObj || !angular.isObject(labelObj)) return '';
+
+		var html = '<label ';
+		html += getClassesStr(labelObj.classes);
+		html += getAttributesStr(labelObj.attributes);
+		html += '>' + labelObj.name + ' ';
+		html += fieldHtml;
+		html += '</label>';
+
+		return html;	
+	}
+
+	function getClassesStr(classArr) {
+		classArr = classArr || [];
+
+		var str = 'class="';
+		for (var i = 0; i < classArr.length; i++) {
+			if (i != 0) {
+				str += ' '; 
+			}
+			str += classArr[i];
+		}
+		str += '" ';
+
+		return str;	
+	}
+
+	function getAttributesStr(attrsObj) {
+		attrsObj = attrsObj || {};
+
+		var str = '';
+		for (var key in attrsObj) {
+			str += getAttributeStr(key, attrsObj[key]) + ' ';
+		}
+
+		return str;
+	}
+
+	function getAttributeStr(key, value) {
+		if (!key || !value) return '';
+		return key + '="' + value + '"';
+	}
+
+	return formFactory;
+});
+
+formGenerator.directive('formGenerator', function($compile, FormFactory) {
+	var linker = function(scope, elm, attrs) {
+
+		scope.addToCheckboxArray = function(value, model) {	
+			var list = scope.$eval(model);
+
+			if (!(list instanceof Array)) {
+				var props = model.split('.');
+				var currentObj = scope[props[0]];
+				var finalProp = props[props.length -1];
+
+				for (var i = 1; i < props.length - 1; i++) { 
+					var objLoop = currentObj[props[i]];
+					
+					if (objLoop === undefined) {
+						var propName = props[i];
+						currentObj[propName] = {};
+						objLoop = currentObj[propName];
+					}
+					
+					currentObj = objLoop;
+				}
+
+				list = [];
+				currentObj[finalProp] = list;
+			}
+
+			if (list.contains(value)) {
+				list.remove(value);
+			} else {
+				list.push(value);
+			}
+		};
+
+		scope.compileForm = function() {
+			var formHtml = FormFactory.getForm(scope.form);
+			elm.html(formHtml);
+			$compile(elm.contents())(scope); 
+		};
+		scope.compileForm();
+
+
+		scope.$watch('form', function(newValue, oldValue) {
+			if (newValue) {
+				scope.compileForm();
+			}
+		}, true);
+	};
+
+	return {
+		restrict: 'EA',
+		replace: true,
+		scope: true,
+		link: linker
+	}
+});
+
+// Remove these and implement differently.
+function contains(a, obj) {
+	var i = this.length;
+	while (i--) {
+		if (this[i] == obj) return true;
+	}
+	return false;
+};
+
+Array.prototype.remove = function() {
+	var what, a = arguments, L = a.length, ax;
+	while (L && this.length) {
+		what = a[--L];
+		while ((ax = this.indexOf(what)) !== -1) {
+			this.splice(ax, 1);
+		}
+	}
+	return this;
+};
